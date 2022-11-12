fae1bad7c3923f308e011814b3447b74d905e9bd
Matheus Henrique Nascimento <110841402+Matheus-Henrique-Nascimento@users.noreply.github.com>
2022-11-12 20:06:36 -0300

    Atualizações.

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -2,27 +2,12 @@ import React, { useState } from 'react';
 import './Card.css';
 
 export default function Card(props){
-
-    const [cardModal, setCardModal] = useState(false);
-    const toggleCardModal = () => {
-        setCardModal(!cardModal);
-    };
-
     return(
         <section className='card'>
             <h3>{props.title}</h3>
-            <img src={props.image} alt={props.alt} onClick={toggleCardModal}/>
+            <img src={props.image} alt={props.alt}/>
             <p>{props.paragraph}</p>
-            <button onClick={toggleCardModal}>Leia Mais...</button>
-            {cardModal && (
-                <section className='cardModal' onClick={toggleCardModal}>
-                    <section className='cardModalContent'>
-                        <h3>Brillant V6</h3>
-                        <p>Não haverá a implmentação de uma NewsLetter nesse projeto. A ideia é puramente estética.</p>
-                        <button onClick={toggleCardModal}>Fechar</button>
-                    </section>
-                </section>
-            )}
+            <button>Leia Mais...</button>
         </section>
     );
 }
---
+++ src/components/cardPopup/CardPopup.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function CardPopup(){
+    return(
+        <section>
+            <h3>Teste</h3>
+            <p>Teste Teste Teste</p>
+        </section>
+    );
+}
